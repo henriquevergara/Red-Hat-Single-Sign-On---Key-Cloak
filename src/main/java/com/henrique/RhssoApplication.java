a4d97f38f8728e6f3ba84ec74a42684e30bec1ca
Henrique <henrique.vergara.hv@gmail.com>
2021-04-20 18:17:42 -0300

    Configuration of KeyCloak

---
+++ src/main/java/com/henrique/RhssoApplication.java
@@ -0,0 +1,13 @@
+package com.henrique;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RhssoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RhssoApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/henrique/TestController.java
@@ -0,0 +1,38 @@
+package com.henrique;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.security.RolesAllowed;
+
+@RestController
+@RequestMapping("/test")
+public class TestController {
+
+
+    @GetMapping(value = "/anonymous")
+    public ResponseEntity<String> getAnonymous(){
+        return ResponseEntity.ok("Hello Anonymous");
+    }
+
+    @RolesAllowed("user")
+    @GetMapping(value = "/user")
+    public ResponseEntity<String> getUser() {
+        return ResponseEntity.ok("Hello User");
+    }
+
+    @RolesAllowed("admin")
+    @GetMapping(value = "/admin")
+    public ResponseEntity<String> getAdmin() {
+        return ResponseEntity.ok("Hello Admin");
+    }
+
+    @RolesAllowed({ "admin", "user" })
+    @GetMapping(value = "/all-user")
+    public ResponseEntity<String> getAllUser() {
+        return ResponseEntity.ok("Hello All User");
+    }
+
+}
